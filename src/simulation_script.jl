eebbc7d0c5f0a5aa07fe2003f5805d58095a2a72
Samuel Dick <s.dick1@uqconnect.edu.au>
2023-10-13 11:12:03 +1000

    Added structs for specific events. RV's for duration of arrival, service and breakdown. skeleton for processing these events.

--- src/simulation_script.jl
+++ src/simulation_script.jl
@@ -1,4 +1,4 @@
-using DataStructures
+using DataStructures, Random, Distributions
 import Base: isless
 
 # State and Event abstract (superclass) types
@@ -24,7 +24,20 @@ function process_event end
 
 #Generic events that we can always use
 struct EndSimEvent <: Event end #Event that ends the simulation
+
 struct LogStateEvent <: Event end #Record when an event happens
+
+struct ExternalArrivalEvent <: Event 
+    q::Int #The index of the queue where job goes
+end 
+ 
+struct EndOfServiceAtQueueEvent <: Event
+    q::Int #The index of the queue where service finished
+end
+
+struct BreakdownEvent <: Event 
+    q::Int #The index of the queue where breakdown has occured 
+end
 
 function process_event(time::Float64, state::State, es_event::EndSimEvent)
     println("Ending simulation at time $time.")
@@ -39,8 +52,9 @@ end
 
 struct QueueNetworkParameters
     num_queues::Int # The number of queues in our system 
-    α::Vector{Any} # External arrival rates (αᵢ's) for queues as an ordered vector
-    μ::Vector{Any} # Service rates (μᵢ's) as an ordered vector
+    α_rates::Vector{Any} # External arrival rates (αᵢ's) for queues as an ordered vector
+    μ_rates::Vector{Any} # Service rates (μᵢ's) as an ordered vector
+    γ_rates::Vector{Any} # On and off paramters 
     scv_arr::Vector{Any} # Squared coefficient of variation of the service processes
 end
 
@@ -53,13 +67,49 @@ end
 total_in_system(state::TandemQueueNetworkState) = sum(state.queues)
 
 #next_arrivival_duration RV for next arrival to the system externally 
+# In this project we'll consider the durations of times between external arrivals to be exponentially distributed
+next_arrival_duration(s::State, q::Int) = rand(Exponential(1/s.params.α_rates[q]))
+
 #next_service_duration RV for next service of the system 
+#As for the durations of service times we will set them as gamma distributed with a ratio of the variance and the mean squared 
+next_service_duration(s::State, q::Int) = rand(rate_scv_gamma(s.params.μ_rates[q], s.params.scv_arr[q]))
+
+#next_breakdown_duration RV for next breakdown of queue in system 
+# Specifically the server changes between on and off and back as follows: 
+# on durations are exponentially distributed with mean γ₁^{-1} where γ₁>0
+next_breakdown_duration(s::State, q::Int) = rand(Exponential(1/s.params.γ_rates[q]))
 
 #PROCESSING EVENTS 
 #process arrival event (external to system)
+function process_event(time::Float64, state::State, arrival_event::ExternalArrivalEvent)
+    ### access trans matrix to work out which queue we add to 
+    ### add person to queue 
+    ### record a new timed event 
+    ### prepare next arrival 
+    ### something specific -> engage server if first job? 
+end
+
 #process end of service event 
-#process a breakdown of a server 
+function process_event(time::Float64, state::State, eos_event::EndOfServiceAtQueueEvent)
+    ### access queue where service has occured 
+    ### record a new timed event 
+    ### remove the job from the queue
+    ### if another customer in queue, then start new service 
     
+    ### transition matrix to work out which queue (or exit) job will go 
+    ### add job to new queue 
+    ###     if queue has no jobs, start service event (record new timed event)
+    ###     else add to end of queue (record new timed event)
+end 
+
+#process a breakdown of a server 
+function process_event(time::Float64, state::State, brk_event::BreakdownEvent)
+    ### access queue where breakdown has occured 
+    ### record a new timed event 
+    ### within queue where breakdown has occured, pause serving ????
+    ### record time for server to come back online???
+end
+
 
 """
 The main simulation function gets an initial state and an initial event
@@ -72,8 +122,10 @@ function simulate(init_state::State, init_timed_event::TimedEvent
                     log_times::Vector{Float64} = Float64[],
                     callback = (time, state) -> nothing)
 
+
     # The event queue
     priority_queue = BinaryMinHeap{TimedEvent}()
+
 
     # Put the standard events in the queue
     push!(priority_queue, init_timed_event)
@@ -114,3 +166,14 @@ function simulate(init_state::State, init_timed_event::TimedEvent
         callback(time, state)
     end
 end;
+
+
+#######################################
+#######################################
+# HELPER FUNCTIONS 
+#######################################
+#######################################
+"""
+A convenience function to make a Gamma distribution with desired rate (inverse of shape) and SCV.
+"""
+rate_scv_gamma(desired_rate::Float64, desired_scv::Float64) = Gamma(1/desired_scv, desired_scv/desired_rate)
