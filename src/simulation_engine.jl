fa9d7cbfa4d72c06ea0553d48f8b813412da535b
Samuel Dick <s.dick1@uqconnect.edu.au>
2023-10-15 14:43:10 +1000

    Restructuring of files. Additional commenting.

---
+++ src/simulation_engine.jl
@@ -0,0 +1,79 @@
+"""
+The main simulation function gets an initial state and an initial event
+that gets things going. Optional arguments are the maximal time for the
+simulation, times for logging events, and a call-back function.
+"""
+function simulate(init_state::State, init_timed_event::TimedEvent
+                    ; 
+                    max_time::Float64 = 10.0, 
+                    log_times::Vector{Float64} = Float64[],
+                    callback = (time, state) -> nothing)
+
+
+    # The event queue
+    priority_queue = BinaryMinHeap{TimedEvent}()
+
+    # Put the standard events in the queue
+    push!(priority_queue, init_timed_event)
+    push!(priority_queue, TimedEvent(EndSimEvent(), max_time))
+    for log_time in log_times
+        push!(priority_queue, TimedEvent(LogStateEvent(), log_time))
+    end
+
+    # initialize the state
+    state = deepcopy(init_state)
+    time = 0.0
+
+    # Callback at simulation start
+    callback(time, state)
+
+    # The main discrete event simulation loop - SIMPLE!
+    while true
+        # Get the next event
+        timed_event = pop!(priority_queue)
+
+        # Advance the time
+        time = timed_event.time
+
+        # Act on the event
+        new_timed_events = process_event(time, state, timed_event.event) 
+
+        # If the event was an end of simulation then stop
+        if timed_event.event isa EndSimEvent
+            break 
+        end
+
+        # The event may spawn 0 or more events which we put in the priority queue 
+        for nte in new_timed_events
+            push!(priority_queue,nte)
+        end
+
+        # Callback for each simulation event
+        callback(time, state)
+    end
+end;
+
+"""
+Runs a discrete event simulation of an Open Generalized Jackson Network with Breakdowns and Repairs `net`. 
+
+The simulation runs from time `0` to `max_time`. 
+
+Statistics about the total mean queue lengths are recorded from `warm_up_time` 
+onwards and the estimated value is returned. For debug purposes statistics about proportion of time on are also recorded. 
+
+This simulation does NOT keep individual customers state, it only keeps the following minimal state: 
+* The number of jobs in each queue.
+* If each server is on or off.
+* In case the server is off and there is a job "stuck" in it, how much processing time is left on that job. 
+"""
+function sim_net(net::NetworkParameters; max_time = 10^6, warm_up_time = 10^4, seed::Int64 = 42)::Float64
+    
+    #Set the random seed
+    Random.seed!(seed)
+    
+    #The function would execute the simulation here - and this would use multiple other functions, types, and files
+
+    estimated_total_mean_queue_length = 12.3 #the function would estimate this value (12.3 is just a place-holder)
+
+    return estimated_total_mean_queue_length
+end;
--- src/simulation_script.jl
+++ src/simulation_script.jl
@@ -5,16 +5,16 @@ import Base: isless
 abstract type Event end
 abstract type State end
 
-#Captures an event and the time it takes place
+# Captures an event and the time it takes place
 struct TimedEvent
     event::Event
     time::Float64
 end
 
-#Comparison of two timed events - this will allow us to use them in a heap/priority-queue
+# Comparison of two timed events - this will allow us to use them in a heap/priority-queue
 isless(te1::TimedEvent, te2::TimedEvent) = te1.time < te2.time
 
-#This is an abstract function 
+# This is an abstract function 
 """
 It will generally be called as 
        new_timed_events = process_event(time, state, event)
@@ -22,33 +22,27 @@ It will generate 0 or more new timed events based on the current event
 """
 function process_event end
 
-#Generic events that we can always use
-struct EndSimEvent <: Event end #Event that ends the simulation
+# Generic events that we can always use
+struct EndSimEvent <: Event end # Event that ends the simulation
 
-struct LogStateEvent <: Event end #Record when an event happens
+struct LogStateEvent <: Event end # Record when an event happens
 
 struct ExternalArrivalEvent <: Event 
-    q::Int #The index of the queue where job goes
+    q::Int # The index of the queue where job goes
 end 
  
 struct EndOfServiceAtQueueEvent <: Event
-    q::Int #The index of the queue where service finished
+    q::Int # The index of the queue where service finished
 end
 
 struct BreakdownEvent <: Event 
-    q::Int #The index of the queue where breakdown has occured 
+    q::Int # The index of the queue where breakdown has occured 
 end
-
-function process_event(time::Float64, state::State, es_event::EndSimEvent)
-    println("Ending simulation at time $time.")
-    return []
-end
-
-function process_event(time::Float64, state::State, ls_event::LogStateEvent)
-    println("Logging state at time $time.")
-    println(state)
-    return []
-end
+###############################
+###############################
+# Network Parameters & State 
+###############################
+###############################
 
 struct QueueNetworkParameters
     num_queues::Int # The number of queues in our system 
@@ -63,15 +57,19 @@ mutable struct QueueNetworkState <: State
     params::QueueNetworkParameterse # Parameters of queue network system
 end 
 
-#Total number of jobs in the system (necessary???)
-total_in_system(state::TandemQueueNetworkState) = sum(state.queues)
+###################################################
+###################################################
+# Random Variables for arrivals, services 
+# and breakdowns. 
+###################################################
+###################################################
 
-#next_arrivival_duration RV for next arrival to the system externally 
+# next_arrivival_duration RV for next arrival to the system externally 
 # In this project we'll consider the durations of times between external arrivals to be exponentially distributed
 next_arrival_duration(s::State, q::Int) = rand(Exponential(1/s.params.α_rates[q]))
 
-#next_service_duration RV for next service of the system 
-#As for the durations of service times we will set them as gamma distributed with a ratio of the variance and the mean squared 
+# next_service_duration RV for next service of the system 
+# As for the durations of service times we will set them as gamma distributed with a ratio of the variance and the mean squared 
 next_service_duration(s::State, q::Int) = rand(rate_scv_gamma(s.params.μ_rates[q], s.params.scv_arr[q]))
 
 #next_breakdown_duration RV for next breakdown of queue in system 
@@ -79,7 +77,25 @@ next_service_duration(s::State, q::Int) = rand(rate_scv_gamma(s.params.μ_rates[
 # on durations are exponentially distributed with mean γ₁^{-1} where γ₁>0
 next_breakdown_duration(s::State, q::Int) = rand(Exponential(1/s.params.γ_rates[q]))
 
-#PROCESSING EVENTS 
+###############################
+###############################
+# PROCESSING EVENTS 
+###############################
+###############################
+
+#Process end of simulation event 
+function process_event(time::Float64, state::State, es_event::EndSimEvent)
+    println("Ending simulation at time $time.")
+    return []
+end
+
+#Process log state event
+function process_event(time::Float64, state::State, ls_event::LogStateEvent)
+    println("Logging state at time $time.")
+    println(state)
+    return []
+end
+
 #process arrival event (external to system)
 function process_event(time::Float64, state::State, arrival_event::ExternalArrivalEvent)
     ### access trans matrix to work out which queue we add to 
@@ -111,63 +127,6 @@ function process_event(time::Float64, state::State, brk_event::BreakdownEvent)
 end
 
 
-"""
-The main simulation function gets an initial state and an initial event
-that gets things going. Optional arguments are the maximal time for the
-simulation, times for logging events, and a call-back function.
-"""
-function simulate(init_state::State, init_timed_event::TimedEvent
-                    ; 
-                    max_time::Float64 = 10.0, 
-                    log_times::Vector{Float64} = Float64[],
-                    callback = (time, state) -> nothing)
-
-
-    # The event queue
-    priority_queue = BinaryMinHeap{TimedEvent}()
-
-
-    # Put the standard events in the queue
-    push!(priority_queue, init_timed_event)
-    push!(priority_queue, TimedEvent(EndSimEvent(), max_time))
-    for log_time in log_times
-        push!(priority_queue, TimedEvent(LogStateEvent(), log_time))
-    end
-
-    # initialize the state
-    state = deepcopy(init_state)
-    time = 0.0
-
-    # Callback at simulation start
-    callback(time, state)
-
-    # The main discrete event simulation loop - SIMPLE!
-    while true
-        # Get the next event
-        timed_event = pop!(priority_queue)
-
-        # Advance the time
-        time = timed_event.time
-
-        # Act on the event
-        new_timed_events = process_event(time, state, timed_event.event) 
-
-        # If the event was an end of simulation then stop
-        if timed_event.event isa EndSimEvent
-            break 
-        end
-
-        # The event may spawn 0 or more events which we put in the priority queue 
-        for nte in new_timed_events
-            push!(priority_queue,nte)
-        end
-
-        # Callback for each simulation event
-        callback(time, state)
-    end
-end;
-
-
 #######################################
 #######################################
 # HELPER FUNCTIONS 
@@ -177,3 +136,6 @@ end;
 A convenience function to make a Gamma distribution with desired rate (inverse of shape) and SCV.
 """
 rate_scv_gamma(desired_rate::Float64, desired_scv::Float64) = Gamma(1/desired_scv, desired_scv/desired_rate)
+
+# Total number of jobs in the system (necessary???)
+total_in_system(state::TandemQueueNetworkState) = sum(state.queues)
