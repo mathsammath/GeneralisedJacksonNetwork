aa8c238e8a10f130ce7b22d56135ce2ce9d42b3f
Samuel Dick <s.dick1@uqconnect.edu.au>
2023-10-17 19:12:11 +1000

    Test three completed. Range of R-values increased. Commenting improved.

--- test/test_three.jl
+++ test/test_three.jl
@@ -3,11 +3,12 @@ A test to investigate the long term proportion of time "on" for each server
 with various set values of R.
 """
 function test_three(net::NetworkParameters)
-    R_vals = [0.1, 0.4, 0.6, 1.0] #R-values to test
+    R_vals = [i*0.1 for i in 1:9] # R-values to test
+    cs, ρ = 0.5, 0.5 # c_s and ρ values to be constant
     on_prop = [] #Long term proportion of time "on" associated with above
     for r in R_vals
-        #NOTE: ρ value currently set to 0.5. What should this be set to?
-        new_sim = set_scenario(net, 0.5, 1.0, r)
+        new_sim = set_scenario(net, ρ, cs, r) # set new senario based on params 
+        # Simulate and push rounded long term proportions into on_prop 
         push!(on_prop, [round(x, digits = 3) for x in sim_net(new_sim, max_time = 1000.0, warm_up_time = 10)[2]])
     end
     return on_prop
