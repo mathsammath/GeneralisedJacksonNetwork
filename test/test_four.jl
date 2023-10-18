de93d00b677da727d32224c0db9d7ae084879e8d
Samuel Dick <s.dick1@uqconnect.edu.au>
2023-10-18 20:42:46 +1000

    Changes involving breakdowns.

--- test/test_four.jl
+++ test/test_four.jl
@@ -2,7 +2,7 @@
 Qualitative test for relation between R and mean queue lengths 
 """
 function test_four(net::NetworkParameters)
-    R = [i*0.025 for i in 1:40] # R-values to test
+    R = [i*0.1 for i in 1:9] # R-values to test
     cs, ρ = 0.5, 0.5 # cₛ and ρ values to remain constant
     mean_queue_lengths = []  
     for r in R 
@@ -15,5 +15,5 @@ function test_four(net::NetworkParameters)
     # Plot simulated mean queue lengths on same plot 
     plot(R, mean_queue_lengths, 
         xlabel = "R", ylabel = "Total mean queue lengths",
-        label = "Simulated", lw = 2, xlim = (0,1),ylim=(0,3000)) 
+        label = "Simulated", lw = 2, xlim = (0,1),ylim=(0,20000)) 
 end 
