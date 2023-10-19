079f469ea6f3bf1188ac760cfc21def8dbfee518
Samuel Dick <s.dick1@uqconnect.edu.au>
2023-10-19 10:21:06 +1000

    Test 1 output now has absolute error plotted.

--- test/test_one.jl
+++ test/test_one.jl
@@ -9,22 +9,26 @@ function test_one(net::NetworkParameters)
         # Set new scenario based on ρ*
         new_net = set_scenario(net, ρ)
         # Simulate for above and record simulated mean queue length
-        push!(mean_lengths, sim_net(new_net, max_time = Float64(10^5), warm_up_time = 10^3)[1])
+        push!(mean_lengths, sim_net(new_net, max_time = Float64(10^6), warm_up_time = 10^4)[1])
     end 
 
     abs_err = [] # Absolute error between theoretical and simulated 
     for i in 1:length(ρ_grid)
         push!(abs_err, abs(theoretical_ρ[i] - mean_lengths[i]))
     end 
-    println("The maximum absolute relative error for total mean queue lengths across ρ* is given by ", maximum(abs_err))
-    println("And the minimum is given by ", minimum(abs_err))
 
     # Plot simulated mean queue length 
-    plt = plot(ρ_grid, mean_lengths, 
+    plt1 = plot(ρ_grid, mean_lengths, 
         xlabel = "ρ*", ylabel = "Total steady state mean queue lengths",
         label = "Simulated", lw = 2, c = :black, xlim = (0,1),ylim=(0,20)) 
     # Plot, on same axis, theoretical mean queue length 
     plot!(ρ_grid, theoretical_ρ, 
         xlabel = "ρ*", ylabel = "Total steady state mean queue lengths",
         label = "Theoretical", lw = 2, linecolour = :red, xlim = (0,1),ylim=(0,20)) 
+
+    plt2 = plot(ρ_grid, abs_err, 
+        xlabel = "ρ*", ylabel = "Absolute Error",
+        label = "Simulated", lw = 2, xlim = (0,1), ylim=(0,2)) 
+        
+    plot(plt1, plt2, size = (1000,600))
 end 
